4eb771c303bb32039a5c2ad9794845b1ecacde36
CiprianR <133102658+Cipick23@users.noreply.github.com>
2024-03-30 20:41:51 +0200

    backup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -37,70 +37,108 @@ const Header = () => {
 
   return (
     <div className={css.header}>
-      <div className={css.sidebarBrandBox}>
-        <div className={css.boxModel}>
-          <svg
-            width="20"
-            height="20"
-            viewBox="0 0 25 23"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+      <div className={css.boxModelLogo}>
+        <svg
+          width="27"
+          height="26"
+          viewBox="0 0 27 26"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g
+            id="&#208;&#187;&#208;&#190;&#208;&#179;&#208;&#190; Money Guard 2"
+            clipPath="url(#clip0_34_6804)"
           >
-            <g clipPath="url(#clip0_17_848)">
+            <g id="&#208;&#161;&#208;&#187;&#208;&#190;&#208;&#185;_x0020_1">
               <path
+                id="Vector"
                 fillRule="evenodd"
                 clipRule="evenodd"
-                d="M17.0686 3.08869C15.4052 2.69093 13.7419 1.89542 12.4944 0.702148C11.2469 1.89542 9.58351 2.69093 7.92017 3.08869C8.336 6.6685 9.58351 9.05504 12.4944 11.0438C15.4052 9.05504 17.0686 6.6685 17.0686 3.08869Z"
-                fill="#FFB627"
+                d="M18.2328 3.50029C16.4298 3.04956 14.6269 2.1481 13.2747 0.795898C11.9225 2.1481 10.1196 3.04956 8.31665 3.50029C8.76738 7.55689 10.1196 10.2613 13.2747 12.5149C16.4298 10.2613 18.2328 7.55689 18.2328 3.50029Z"
+                fill="#FFC727"
               />
               <path
+                id="Vector_2"
                 fillRule="evenodd"
                 clipRule="evenodd"
-                d="M15.4051 17.8989L3.76172 4.77295V11.1371L12.9101 21.081L15.4051 17.8989Z"
+                d="M16.4296 20.2824L3.80908 5.4082V12.6199L13.7252 23.8882L16.4296 20.2824Z"
                 fill="#FBFBFB"
               />
               <path
+                id="Vector_3"
                 fillRule="evenodd"
                 clipRule="evenodd"
-                d="M16.653 16.7058L21.6431 11.1372V5.1709L13.7422 13.9215L16.653 16.7058Z"
+                d="M17.782 18.9306L23.1908 12.6204V5.85938L14.6268 15.7755L17.782 18.9306Z"
                 fill="#FBFBFB"
               />
               <path
+                id="Vector_4"
                 fillRule="evenodd"
                 clipRule="evenodd"
-                d="M17.0688 18.6945V22.2743L21.643 17.1035V13.5237L17.0688 18.6945Z"
+                d="M18.2327 21.1842V25.2408L23.1907 19.3813V15.3247L18.2327 21.1842Z"
                 fill="#FBFBFB"
               />
               <path
+                id="Vector_5"
                 fillRule="evenodd"
                 clipRule="evenodd"
-                d="M8.33592 18.6945L3.76172 13.5237V17.1035L8.33592 22.2743V18.6945Z"
+                d="M8.76714 21.1842L3.80908 15.3247V19.3813L8.76714 25.2408V21.1842Z"
                 fill="#FBFBFB"
               />
             </g>
-            <defs>
-              <clipPath id="clip0_17_848">
-                <rect
-                  width="23.4894"
-                  height="22.4681"
-                  fill="white"
-                  transform="translate(0.95752)"
-                />
-              </clipPath>
-            </defs>
-          </svg>
-          <span>Money Guard</span>
-        </div>
+          </g>
+          <defs>
+            <clipPath id="clip0_34_6804">
+              <rect
+                width="25.4605"
+                height="25.4605"
+                fill="white"
+                transform="translate(0.769775)"
+              />
+            </clipPath>
+          </defs>
+        </svg>
+        <span className={css.baseTypography}>Money Guard</span>
       </div>
-      <ul className={css.sidebarUl}>
-        <li className={css.listLi}>
-          <NavLink to="/" className={css.navLink}>
+      <ul className={css.box}>
+        <nav className={css.list}>
+          <NavLink to="/" className={css.userNameHeader}>
+            name
             {user.name}
           </NavLink>
-          <button type="button" onClick={handleExitClick}>
-            Exit
-          </button>
-        </li>
+
+          <NavLink
+            className={css.exitBtn}
+            type="button"
+            onClick={handleExitClick}
+          >
+            <span>
+              <svg
+                width="2"
+                height="30"
+                viewBox="0 0 2 30"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M1 0V30" stroke="white" strokeOpacity="0.6" />
+              </svg>
+            </span>
+            <svg
+              width="18"
+              height="18"
+              viewBox="0 0 18 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M11.2788 13.0708H12.6844V15.8818C12.6844 17.0443 11.7386 17.99 10.5761 17.99H2.10814C0.945786 17.99 0 17.0443 0 15.8818V2.10814C0 0.945786 0.945786 0 2.10814 0H10.5761C11.7386 0 12.6844 0.945786 12.6844 2.10814V4.91913H11.2788V2.10814C11.2788 1.72073 10.9637 1.40543 10.5761 1.40543H2.10814C1.72073 1.40543 1.40543 1.72073 1.40543 2.10814V15.8818C1.40543 16.2692 1.72073 16.5845 2.10814 16.5845H10.5761C10.9637 16.5845 11.2788 16.2692 11.2788 15.8818V13.0708ZM14.6872 5.68213L13.6934 6.67598L15.3096 8.29234H6.21922V9.69777H15.3096L13.6934 11.314L14.6872 12.3078L18 8.99506L14.6872 5.68213Z"
+                fill="white"
+                fillOpacity="0.6"
+              />
+            </svg>
+            <span className={css.exitBtn}>Exit</span>
+          </NavLink>
+        </nav>
       </ul>
       {/* {isModalOpen && (
         <Modal onClose={handleCloseModal}>
--- src/pages/DashboardPage/Dashboard.js
+++ src/pages/DashboardPage/Dashboard.js
@@ -1,6 +1,7 @@
 import Header from 'components/Header/Header';
 import { Navigation } from 'components/Navigation/Navigation';
 import Home from 'pages/HomePage/Home';
+// import css from './Dashboard.module.css';
 
 const Dashboard = () => {
   return (
