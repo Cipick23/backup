60f72f88e23bf90e19b3e83017055dbcb53315b0
CiprianR <133102658+Cipick23@users.noreply.github.com>
2024-06-01 15:38:46 +0300

    backup hw03-mongodb

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
+import contactsRouter from "./routes/api/contacts.js";
+import connectToDb from "./utils/connectToDb.js";
+
+export const STATUS_CODES = {
+  success: 200,
+  deleted: 204,
+  error: 500,
+};
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+connectToDb();
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+});
+
+export default app;
